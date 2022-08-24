d881f07627fab56c015262bc06dc8941ae4a84a6
gitjhonatan <jhonatan.lima105@gmail.com>
2022-08-24 15:33:22 -0300

    :sparkles: (redis) adicionado cache de cnpj

--- cnpj.ts
+++ cnpj.ts
@@ -5,16 +5,40 @@ import LambdaHTTPResponse from "./interfaces/lambda-response.interface";
 import receitawsService from "./services/receitaws.service";
 import AppConfig from './config/index'
 import MongoRegistro from './interfaces/mongo-registro.interface'
+import { validateCNPJ } from 'validations-br';
+import { Mongoose } from "mongoose";
 
 export const post: Handler = async (req) => {
     AppConfig.load()
     const { id, name, cnpj } = JSON.parse(req.body)
+    const numeros_cnpj = cnpj.replace(/\D+/g, '')
     if (!id || !name || !cnpj)
         return new Promise((resolve) => {
             resolve({
-                statusCode: 400,
                 body: JSON.stringify({
                     "message": "Faltando campos obrigatórios"
+                })
+            });
+        });
+
+    if (!validateCNPJ(numeros_cnpj))
+        return new Promise((resolve) => {
+            resolve({
+                body: JSON.stringify({
+                    "message": "CNPJ inválido"
+                })
+            });
+        });
+
+
+    const mongo_select = await CNPJModel.findOne({ id, name, cnpj: numeros_cnpj })
+
+    if (mongo_select)
+        return new Promise((resolve) => {
+            resolve({
+                body: JSON.stringify({
+                    "message": "Registro já postado",
+                    "data": mongo_select
                 })
             });
         });
@@ -22,31 +46,29 @@ export const post: Handler = async (req) => {
     const redis_objeto = {
         id,
         name,
-        cnpj,
+        cnpj: numeros_cnpj,
         status: 'Aguardando Processamento',
         data: {}
     }
-    const mongo_insert = await CNPJModel.create(redis_objeto)
 
+    const { _id } = await CNPJModel.create(redis_objeto)
 
     const response = {
-        statusCode: 200,
-        body: req.body
+        body: JSON.stringify({ _id, ...JSON.parse(req.body) })
     };
-
-    // await redis.add(id, redis_objeto)
-
     return new Promise((resolve) => {
         resolve(response);
     });
 };
 
 export const get: Handler = async (req) => {
+    AppConfig.load()
+
     let message: string
     let status_code: number
 
     const id = req.queryStringParameters?.id
-    const registro: object = id ? JSON.parse(await redis.get(id)) : {}
+    const registro: object | null = id ? await CNPJModel.findOne({ _id: id }) : {}
 
     if (!id) {
         status_code = 400
@@ -65,11 +87,8 @@ export const get: Handler = async (req) => {
 
     return new Promise((resolve) => {
         resolve(JSON.stringify({
-            statusCode: status_code,
-            body: {
-                message,
-                data: registro
-            }
+            message,
+            data: registro || {}
         }))
     });
 };
@@ -81,24 +100,17 @@ export const popula: Handler = async (req) => {
 
     // a api é free e só aceita 3 requisições por minuto
     cnpjs_aguardando.forEach(async (registro_aguardando: MongoRegistro) => {
-        console.log("processando")
-        const cnpj_info = await receitawsService.getCNPJInfo(registro_aguardando.cnpj)
+        const cnpj_cache = await redis.get(registro_aguardando.cnpj)
+        const cnpj_info = cnpj_cache ? JSON.parse(cnpj_cache) : await receitawsService.getCNPJInfo(registro_aguardando.cnpj)
         const registro_processado = {
             ...registro_aguardando.toJSON(),
             data: cnpj_info,
-            status:'OK'
+            status: 'OK'
         }
-        console.log(registro_processado)
-        CNPJModel.updateOne(registro_aguardando.toJSON(), registro_processado)
-    })
 
-    // return new Promise((resolve) => {
-    //     resolve(JSON.stringify({
-    //         statusCode: status_code,
-    //         body: {
-    //             message,
-    //             data: registro
-    //         }
-    //     }))
-    // });
+        if (!cnpj_cache)
+            redis.add(registro_aguardando.cnpj, cnpj_info)
+
+        await CNPJModel.updateOne(registro_aguardando.toJSON(), registro_processado)
+    })
 };
--- handler.ts
+++ handler.ts
@@ -2,7 +2,6 @@ import { Handler } from "aws-lambda";
 
 export const hello: Handler = (event) => {
   const response = {
-    statusCode: 200,
     body: JSON.stringify({
       message: "Serverless Test"
     })
